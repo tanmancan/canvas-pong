a3541f8625144e68c4b4985a38025e9f0d3c7b37
tanmancan <contact@tkarimdesign.com>
2019-08-25 20:11:13 -0400

    Build config for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
   contentBase: path.resolve(__dirname, 'src'),
   outputPath: path.resolve(__dirname, './dist'),
-  publicPath: '/dist/'
-}
+  publicPath: '/dist/',
+};
 
 module.exports = {
   entry: {
     babelpolyfill: 'babel-polyfill',
-    app: './src/app.js'
+    app: './src/app.ts',
   },
   output: {
     filename: '[name].js',
     path: config.outputPath,
-    publicPath: config.publicPath
+    publicPath: config.publicPath,
   },
+  devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
   ],
-  devServer:{
+  devServer: {
     overlay: true,
     disableHostCheck: true,
     hot: true,
     inline: true,
     watchOptions: {
-      poll: true
-    }
+      poll: true,
+    },
   },
   module: {
     rules: [
@@ -43,23 +46,29 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
-          }
-        }
+            presets: ['env'],
+          },
+        },
       },
       {
         test: /\.wav$/,
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name][hash].[ext]'
-          }
-        }
-      }
-    ]
+            name: '[name][hash].[ext]',
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /(node_modules)/,
+      },
+    ],
   },
   resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-    }
+    },
   },
 };
